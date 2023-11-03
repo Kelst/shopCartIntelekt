4c961caed42989a4774c6f432c0fa1b82ee46aa3
Vladyslav Bezkorovainij <seile6558@gmail.com>
2023-11-03 17:00:34 +0200

    ds bwe

--- src/pages/ShopCart.jsx
+++ src/pages/ShopCart.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { useStore } from '../store'
 
 export default function ShopCart() {
+  const goodCart=useStore(state=>state.goodCart)
+  
   return (
-    <div>ShopCart</div>
+    <div>
+
+        {
+          goodCart.map(e=><p>{e.name}</p>)
+        }
+    </div>
   )
 }
