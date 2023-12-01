a58d6060d30ddb90e52c31e3a4aadb26725991d7
Vladyslav Bezkorovainij <seile6558@gmail.com>
2023-12-01 10:56:48 +0200

    dssd

--- src/pages/ReadMe.jsx
+++ src/pages/ReadMe.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import AttachFileOutlinedIcon from '@mui/icons-material/AttachFileOutlined';
 export default function ReadMe() {
   return (
-    <div>
+    <div className='    max-w-[460px] mx-auto'>
         <h2 className="text-2xl mb-4 font-medium">Умови доставки та оплати</h2>
         <div class="max-w-2xl mx-auto p-4">
   <h2 class="text-xl font-bold mb-4">Способи доставки:</h2>
@@ -19,6 +19,14 @@ export default function ReadMe() {
     <p class="text-sm">м. Чернівці, вул. Головна, 265а, ТРЦ «DEPOt» (2-й поверх)</p>
   </div>
 </div>
+
+
+  <h2 class="text-xl font-bold mb-4">Способи оплати:</h2>
+
+  <div class="mb-6">
+    <p class="text-sm">При замовленні доставки товару "Новою Поштою" - обов'язкова 100% передоплата замовлення.  Вартість доставки замовлення оплачує покупець, згідно тарифів "Нової Пошти".</p>
+    <p class="text-sm">Доставкою Нова Пошта- 100% предоплата на обраний товар.</p>
+  </div>
     </div>
   )
 }
